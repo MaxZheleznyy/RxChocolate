541745a03658d65192eaff91fe2eb35cfde25a40
Max <maxzheleznyy@gmail.com>
2020-03-16 13:26:29 -0700

    Recreated SceneDelegate file

---
+++ RxChocolate/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  RxChocolate
+//
+//  Created by Maxim Zheleznyy on 3/16/20.
+//  Copyright © 2020 Maxim Zheleznyy. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
